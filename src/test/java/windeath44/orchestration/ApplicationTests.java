74ea756df05d4c3d36cfdd8f95c7c8f179f867cc
yuuki08noah <yuuki08noah@gmail.com>
2025-05-08 16:48:56 +0900

    base :: orchestration server(kafka handling)

---
+++ src/test/java/windeath44/orchestration/ApplicationTests.java
@@ -0,0 +1,13 @@
+package windeath44.orchestration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
