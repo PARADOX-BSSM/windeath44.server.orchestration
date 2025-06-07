3328de66b35ca7b2a5568455f311f4d721532cc7
pdh0128 <pdh0128a@gmail.com>
2025-06-07 16:03:52 +0900

    fix :: topic 이름 변경 BACKEND-25

--- src/main/java/windeath44/orchestration/adapter/in/messaging/MemorialCreateSubscribe.java
+++ src/main/java/windeath44/orchestration/adapter/in/messaging/MemorialCreateSubscribe.java
@@ -2,7 +2,6 @@ package windeath44.orchestration.adapter.in.messaging;
 
 
 import com.example.avro.MemorialAvroSchema;
-import com.google.common.eventbus.Subscribe;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
