0e6be70e22f8e7502f039ba705269eb2f0462fb2
yuuki08noah <yuuki08noah@gmail.com>
2025-07-15 09:58:13 +0900

    fix :: [BACKEND-66] EnableJpaAuditing 제거

--- src/main/java/windeath44/orchestration/Application.java
+++ src/main/java/windeath44/orchestration/Application.java
@@ -2,13 +2,12 @@ package windeath44.orchestration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 @ConfigurationPropertiesScan
-@EnableJpaAuditing
 @EnableKafka
 public class Application {
   public static void main(String[] args) {
