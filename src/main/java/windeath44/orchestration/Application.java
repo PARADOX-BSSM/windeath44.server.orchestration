5fc70d7fad43e8de9522e920fbfe7231b4ca402b
pdh0128 <pdh0128a@gmail.com>
2025-07-17 02:12:51 +0900

    fix :: avro 수정 #BACKEND-69

--- src/main/java/windeath44/orchestration/Application.java
+++ src/main/java/windeath44/orchestration/Application.java
@@ -3,12 +3,12 @@ package windeath44.orchestration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableJpaAuditing
+@EnableMongoAuditing
 @EnableKafka
 public class Application {
   public static void main(String[] args) {
