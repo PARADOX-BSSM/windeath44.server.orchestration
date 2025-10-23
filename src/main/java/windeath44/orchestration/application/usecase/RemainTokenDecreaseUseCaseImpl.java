cca290d049df09c99b7859d51ad26830cf679998
pdh0128 <pdh0128a@gmail.com>
2025-10-23 23:50:07 +0900

    refactor :: decrase-request kafka send를 service로 분리

--- src/main/java/windeath44/orchestration/application/usecase/RemainTokenDecreaseUseCaseImpl.java
+++ src/main/java/windeath44/orchestration/application/usecase/RemainTokenDecreaseUseCaseImpl.java
@@ -3,19 +3,19 @@ package windeath44.orchestration.application.usecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import windeath44.orchestration.application.service.RemainTokenDecreaseService;
 import windeath44.orchestration.domain.mapper.EventMapper;
 import windeath44.orchestration.domain.model.ChatbotChatEvent;
 import windeath44.orchestration.domain.port.in.RemainTokenDecreaseUseCase;
 import windeath44.orchestration.domain.repository.EventRepository;
-import windeath44.orchestration.global.kafka.KafkaProducer;
 import windeath44.server.chatbot.avro.ChatEvent;
 
 @Component
 @RequiredArgsConstructor
 public class RemainTokenDecreaseUseCaseImpl implements RemainTokenDecreaseUseCase {
+    private final RemainTokenDecreaseService remainTokenDecreaseService;
     private final EventMapper eventMapper;
     private final EventRepository eventRepository;
-    private final KafkaProducer kafkaProducer;
 
     @Override
     @Transactional
@@ -25,6 +25,6 @@ public class RemainTokenDecreaseUseCaseImpl implements RemainTokenDecreaseUseCas
         eventRepository.save(chatbotChatEvent);
 
         // remain-token-decrease-request 이벤트 발행
-        kafkaProducer.send("remain-token-decrease-request", chatEvent);
+        remainTokenDecreaseService.execute(chatEvent);
     }
 }
